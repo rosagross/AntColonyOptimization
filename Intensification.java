c080bcd40e1e93364f81f5df6f474c277f21da45
earens <49277057+earens@users.noreply.github.com>
2019-06-01 21:41:34 +0200

    Update Intensification.java

--- Intensification.java
+++ Intensification.java
@@ -3,17 +3,17 @@ package tsp;
 import java.util.Arrays;
 
 public class Intensification {
-
+	
 	private double intensificationParameter;
 	private double[][] pheromoneMatrix;
 	private int[][] distances;
-
+	
 	public Intensification(double intensificationParameter, Problem p) {
 		this.intensificationParameter = intensificationParameter;
 		this.pheromoneMatrix = initWithOnes(p.getNumberTowns());
 		this.distances = p.getTownsDistances();
 	}
-
+	
 	/**
 	 * Initialization for the pheromoneMatrix
 	 * @param size
@@ -30,43 +30,44 @@ public class Intensification {
 
 
 	/**
-	 * Method that intensifies the values of the pheromone matrix
+	 * Method that intensifies the values of the pheromone matrix 
 	 * @param pheromoneMatrix
 	 * @return
 	 */
 	public double[][] intensify(double[][] pheromoneMatrix, int[][] solutions) {
-
+		
 		// find the best solution of the solution array (contains city indexes)
 		int[] bestSolution = findBestSolution(solutions);
 		
-		// we start with the first city so with the index 0
-		pheromoneMatrix[0][bestSolution[0]] *= this.intensificationParameter;
-		// now we manipulate the pheromone matrix
+		// TODO: wo starten wir?? Es muss noch ein weiterer wert veraendert werden (der von der startstadt aus geht)
+		// now we manipulate the pheromone matrix 
+		pheromoneMatrix[0][bestSolution[0]] += this.intensificationParameter;
 		for (int i = 0; i < bestSolution.length -1; i++) {
-			pheromoneMatrix[bestSolution[i]][bestSolution[i+1]] *= this.intensificationParameter;
+			pheromoneMatrix[bestSolution[i]][bestSolution[i+1]] += this.intensificationParameter;
 		}
+		pheromoneMatrix[bestSolution[bestSolution.length - 1]][0] += this.intensificationParameter;
 
-		// update attribute
-		this.pheromoneMatrix = pheromoneMatrix;
-
+		
 		return pheromoneMatrix;
 	}
-
+	
 	/**
 	 * Method finds the best solution based on the distance matrix.
 	 * @return best solution, so the shortest way
 	 */
-	public static int[] findBestSolution(int[][] solutions) {
+	public int[] findBestSolution(int[][] solutions) {
 		// store all the calculated distances in one array
-		int[] distValues = new int[solutions.length];
-
+		int[] distValues = new int[solutions.length]; 
+		
 		for (int i = 0; i < solutions.length; i++) {
 			distValues[i] = evalSolution(solutions[i]); // don't know if method already exists..
 		}
 
+//		Evaluation.printArray(distValues);
+//		System.out.println();
 		// find index array minimum of all the distance values
 		int indexMin = getMin(distValues);
-
+		
 		return solutions[indexMin];
 	}
 
@@ -77,15 +78,27 @@ public class Intensification {
 	 */
 	private static int getMin(int[] inputArray) {
 		// Method for getting the minimum value
-		int minValue = inputArray[0];
+		int minValue = inputArray[0]; 
 		int indexMin = 0;
-		for(int i = 1; i < inputArray.length; i++){
-			if(inputArray[i] < minValue){
-		        minValue = inputArray[i];
+		for(int i = 1; i < inputArray.length; i++){ 
+			if(inputArray[i] < minValue){ 
+		        minValue = inputArray[i]; 
 		        indexMin = i;
-		    }
+		    } 
+		} 
+		return indexMin; 
+		  
+	}
+	
+	public int evalSolution(int[] solutions) {
+		
+		int distValue  = distances[0][solutions[0]];
+		for (int i = 0; i < solutions.length - 1; i++) {
+			distValue += distances[solutions[i]][solutions[i + 1]];
 		}
-		return indexMin;
-
+		distValue += distances[solutions[solutions.length-1]][0];
+		
+		return distValue;
+		
 	}
 }
