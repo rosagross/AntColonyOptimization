4f276f4abb7f2b061febcb7c355c2b0f21c072cd
TulaBoeschen <46481469+TulaBoeschen@users.noreply.github.com>
2019-06-01 11:50:55 +0200

    Change attributes for evaporate()

--- Evaporation.java
+++ Evaporation.java
@@ -31,7 +31,7 @@ public class Evaporation {
 	* @param pheromone_matrix
 	* @param evaporationParameter
 	*/
-	public double[][] evaporate(double[][] pheromone_matrix, double evaporationParameter) {
+	public double[][] evaporate(double[][] pheromone_matrix) {
 		for(int i = 0; i < pheromone_matrix.length; i++) {
 			for(int j = 0; j < pheromone_matrix[1].length; j++) {
 				pheromone_matrix[i][j] = pheromone_matrix[i][j] * evaporationParameter;
