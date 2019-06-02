a236ccfa6c05df7e5bc7d2e435578a702f3c1e21
earens <49277057+earens@users.noreply.github.com>
2019-06-02 20:24:09 +0200

    Update SolutionGeneration.java

--- SolutionGeneration.java
+++ SolutionGeneration.java
@@ -25,6 +25,8 @@ public class SolutionGeneration {
 	
 	private int[][] solutions;
 	
+	private Random random = new Random();
+	
 
 	
 	
@@ -56,111 +58,144 @@ public class SolutionGeneration {
 	public int[][] solutionsMatrix(double[][] pheromoneMatrix) {
 		
 		int[][] solutionsMatrix = new int[ants][distanceMatrix.length - 1];
-		int[] townsUnvisited;
+		int[][] townsUnvisited;
 		double[] probabilities;
 		int counter = 0;
-		int[] choice = new int[10000];
+		int[] choice = new int[1000];
 		int end;
+		int indexLastUnvisitedTowns = distanceMatrix.length - 1;
+		int indexProbabilities = distanceMatrix.length - 1;
 
 		
-		int[] solution = new int[distanceMatrix.length - 1];
-	    Random random = new Random();
+	   
 
 		
-		//create solution for each ant
-		for (int i = 0; i < ants; i++) {
-
-			
-		    int progress = 0;
-			int lastTownIndex = - 1;
-			probabilities = new double[distanceMatrix.length - 1];
-
-
-			
-			//prepare townsUnvisited
-			townsUnvisited = new int[distanceMatrix.length - 1];
-			for (int t = 0; t < distanceMatrix.length - 1; t++) {
-				townsUnvisited[t] = t + 1;
-			}
-			
-			while (townsUnvisited.length > 0) {
-				
-				double normalization = 0.0;
-				//calculate probability for each town that is left
-				for (int j = 0; j < townsUnvisited.length; j++) {
-					
-					//collect probabilities for all left towns in array
-					probabilities[j] = calculateProb(townsUnvisited.length, lastTownIndex +1 , townsUnvisited[j],  pheromoneMatrix);
-					normalization += probabilities[j];
-				}	
-				
-					
-				for (int j2 = 0; j2 < probabilities.length; j2++) {
-					probabilities[j2] =  probabilities[j2] * (1.0/normalization);
-				}
-				
-
-//				System.out.println();
-//				Evaluation.printArray(probabilities);
-//				System.out.println();
-					
-
-					for (int j2 = 0; j2 < probabilities.length; j2++) {
-						
-					
-						if (j2 == townsUnvisited.length -1) {
-							end = choice.length;
-						} else {
-							end = (int) Math.round(probabilities[j2]*10000);
-						}
-						
-	
-						//add current town choices, occurrence of current town depends and probability
-						for (int l = 0; l < end; l++) {
-							choice[counter] = townsUnvisited[j2];
-							
-							
-							if (counter < choice.length - 1) {
-								counter++;
-							} else {
-								break;
-							}
-						}	
-					}
-				
-				
 		
-				//choose town from choice randomly				
-				solution[progress] = choice[random.nextInt(choice.length - 1)];
+	    int progress = 0;
+		int lastTownIndex = - 1;
+		probabilities = new double[distanceMatrix.length - 1];
 
-				for (int j = 0; j < townsUnvisited.length; j++) {
-					if (townsUnvisited[j] == solution[progress]) {
-						lastTownIndex = j;
-						
-						//delete chosen town from list of unvisited towns
-						swap(townsUnvisited, lastTownIndex, townsUnvisited.length - 1);
-						townsUnvisited = Arrays.copyOf(townsUnvisited, townsUnvisited.length-1);
 
-						break;
-					}
-				}
-				
 			
-
-				//shorten probabilities
-				probabilities = Arrays.copyOf(probabilities, probabilities.length-1);
-				
-				//set counters for next round
-				counter = 0;
-				progress++;
-			}
-			
-			//copy solution in solutionMatrix
-			for (int j = 0; j < solution.length; j++) {
-				solutionsMatrix[i][j] = solution[j];
+		//prepare townsUnvisited
+		townsUnvisited = new int[ants][distanceMatrix.length - 1];
+		for (int a = 0; a < ants; a++) {
+			for (int j = 0; j < distanceMatrix.length - 1; j++) {
+				townsUnvisited[a][j] = j + 1;
 			}
 		}
+		
+	
 			
+		for (int i = 0; i < distanceMatrix.length - 1; i++) {
+			
+			
+			for (int a = 0; a < ants; a++) {
+				
+				
+				for (int j = 0; j < indexLastUnvisitedTowns; j++) {
+					
+					if (i == 0) {
+						lastTownIndex = 0;
+					} else {
+						lastTownIndex = solutionsMatrix[a][i-1];
+					}
+					
+					
+//					
+					
+					probabilities[j] = calculateProb(lastTownIndex, townsUnvisited[a][j],  pheromoneMatrix);
+				}
+				
+//				Evaluation.printMatrix(townsUnvisited);
+//				System.out.println();
+				
+				
+				//normalize probabilities
+				double normalization = 0.0;
+				//normalize all probabilities
+				for (int j = 0; j < indexLastUnvisitedTowns; j++) {
+					//calculate normalization value
+					normalization += Math.pow(pheromoneMatrix[lastTownIndex][townsUnvisited[a][j]],pheromoneWeight)/Math.pow(distanceMatrix[lastTownIndex][townsUnvisited[a][j]],heuristicWeights);	
+				}
+				for (int j2 = 0; j2 < indexProbabilities; j2++) {
+					probabilities[j2] =  probabilities[j2] * (1.0/normalization);
+				}
+				 
+//				Evaluation.printArray(probabilities);
+
+				
+				// with probability of q just take the next town with the next probability
+                if (getRandomBoolean(greedyParameter)) {
+                    solutionsMatrix[a][progress] = townsUnvisited[a][getMaxTown(probabilities)];
+                } else { 
+                	
+
+				for (int j2 = 0; j2 < indexProbabilities; j2++) {
+					
+			
+
+					if (j2 == indexProbabilities - 1) {
+						end = choice.length;
+					} else {
+						//System.out.println(j2);
+						end = (int) Math.round(probabilities[j2]*1000);
+					}	
+						
+					
+
+					//add current town choices, occurrence of current town depends and probability
+					for (int l = 0; l < end; l++) {
+
+						choice[counter] = townsUnvisited[a][j2];
+						if (!(counter < choice.length - 1)) {
+							break;
+						} 
+						counter++;
+					}
+					
+					
+				
+				}
+				
+//				System.out.println();
+//				Evaluation.printArray(choice);
+//				System.out.println();
+
+				//set counters for next round
+				counter = 0;
+			
+				//choose town from choice randomly				
+				solutionsMatrix[a][progress] = choice[random.nextInt(choice.length)];
+            }
+				
+				
+                
+                
+			
+				
+				for (int j = 0; j < indexLastUnvisitedTowns; j++) {
+					if (townsUnvisited[a][j] == solutionsMatrix[a][progress]) {
+						
+						
+						//delete chosen town from list of unvisited towns
+						swap(townsUnvisited[a], j, indexLastUnvisitedTowns - 1);
+						break;
+				
+					}
+				}
+				
+				
+				
+					
+			}
+			indexLastUnvisitedTowns--;
+			indexProbabilities--;
+			progress++;
+
+		}
+			
+		
 		return solutionsMatrix;
 	}
 	
@@ -172,14 +207,10 @@ public class SolutionGeneration {
 	 * @param j
 	 * @return probability
 	 */
-	private double calculateProb(double solutionLength, int i, int j,  double[][] pheromoneMatrix) {
+	private double calculateProb(int i, int j,  double[][] pheromoneMatrix) {
 		
-		double solutionSum = 0.0;
-		for (int k = 0; k < pheromoneMatrix.length; k++) {
-			solutionSum += pheromoneMatrix[i][k];
-		}
 		
-		double prob = (1.0 /solutionSum) * (Math.pow(pheromoneMatrix[i][j],pheromoneWeight)/Math.pow(distanceMatrix[i][j],heuristicWeights));
+		double prob = Math.pow(pheromoneMatrix[i][j],pheromoneWeight)/Math.pow(distanceMatrix[i][j],heuristicWeights);
 		
 		
 		
@@ -195,4 +226,26 @@ public class SolutionGeneration {
 		
 		return array;
 	}
+	
+	public boolean getRandomBoolean(double p){
+        return random.nextDouble() < p;
+    }
+	
+	/**
+     * find maximum
+     * @param inputArray
+     * @return
+     */
+    private static int getMaxTown(double[] inputArray) {
+        double maxValue = inputArray[0];
+        int indexMax = 0;
+        for(int i = 1; i < inputArray.length; i++){
+        	if(inputArray[i] > maxValue){
+        		maxValue = inputArray[i];
+        		indexMax = i;
+        	}
+        }
+        return indexMax;
+        
+    }
 }
