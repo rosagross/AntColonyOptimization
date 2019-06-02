b5c1d0da0952a72aaad61ac07b9a8fd9c276aab6
TulaBoeschen <46481469+TulaBoeschen@users.noreply.github.com>
2019-06-02 22:09:58 +0200

    Update SolutionGeneration.java

--- SolutionGeneration.java
+++ SolutionGeneration.java
@@ -6,30 +6,83 @@ import java.util.function.IntBinaryOperator;
 import java.util.function.IntToDoubleFunction;
 import java.util.function.IntUnaryOperator;
 
+/**
+ * This class generates new solutions for every ant.
+ * @author Emilia, Tula, Rosa
+ */
 public class SolutionGeneration {
 
-	
+	/**
+	 * number determining how many ants (i. e. solutions) we have
+	 */
 	private int ants;
+	
+	
 
+	/**
+	 * parameter that decides how important the pheromones are
+	 */
 	private double pheromoneWeight;
 	
+	
+	
+	/**
+	 * parameter that decides how important the heuristic is
+	 */
 	private double heuristicWeights;
 	
+	
+	
+	/**
+	 * double [0, 1] that determines with what probability we select the best value to go to the next city 
+	 */
 	private double greedyParameter;
 	
+	
+	
+	/**
+	 * matrix shaped antsXcities containing possible solutions
+	 */
 	private int[][] solutionsMatrix;
 	
+	
+	
+	/**
+	 * matrix containing current pheromone values
+	 */
 	private double[][] pheromoneMatrix;
 	
+	
+	
+	/**
+	 * matrix containing the distances from each city to any other city
+	 */
 	private int[][] distanceMatrix;
 	
+	
+	
+	/**
+	 * matrix containing all solutions
+	 */
 	private int[][] solutions;
 	
+	
+	
+	/**
+	 * random object needed to find random city and random double
+	 */
 	private Random random = new Random();
 	
 
 	
-	
+	/**
+	 * Constructor for solution generation. Object of this class has the following params:
+	 * @param distanceMatrix
+	 * @param ants
+	 * @param pheromoneWeight
+	 * @param heuristicWeight
+	 * @param greedyParameter
+	 */
 	public SolutionGeneration(int[][] distanceMatrix, int ants, double pheromoneWeight, double heuristicWeight, double greedyParameter) {
 		
 		this.ants = ants;
@@ -37,22 +90,19 @@ public class SolutionGeneration {
 		this.heuristicWeights = heuristicWeight;
 		this.pheromoneWeight = pheromoneWeight;
 		this.greedyParameter = greedyParameter;
-		
-		
 	}
 	
 	/**
-	 * 
+	 * Getter for matrix containing solution paths
 	 * @return matrix containing solution paths
 	 */
 	public int[][] getSolutionsMatrix(){
 		return solutionsMatrix;
 	}
 	
-	
-	
 	/**
-	 * Calculate soultion path for each ant according to the amount of pheromone  and the distances
+	 * Calculate solution path for each ant according to the amount of pheromone  and the distances
+	 * @param pheromone matrix
 	 * @return solutionsMatrix
 	 */
 	public int[][] solutionsMatrix(double[][] pheromoneMatrix) {
@@ -66,17 +116,10 @@ public class SolutionGeneration {
 		int indexLastUnvisitedTowns = distanceMatrix.length - 1;
 		int indexProbabilities = distanceMatrix.length - 1;
 
-		
-	   
-
-		
-		
 	    int progress = 0;
 		int lastTownIndex = - 1;
 		probabilities = new double[distanceMatrix.length - 1];
 
-
-			
 		//prepare townsUnvisited
 		townsUnvisited = new int[ants][distanceMatrix.length - 1];
 		for (int a = 0; a < ants; a++) {
@@ -84,15 +127,9 @@ public class SolutionGeneration {
 				townsUnvisited[a][j] = j + 1;
 			}
 		}
-		
-	
 			
 		for (int i = 0; i < distanceMatrix.length - 1; i++) {
-			
-			
 			for (int a = 0; a < ants; a++) {
-				
-				
 				for (int j = 0; j < indexLastUnvisitedTowns; j++) {
 					
 					if (i == 0) {
@@ -100,17 +137,12 @@ public class SolutionGeneration {
 					} else {
 						lastTownIndex = solutionsMatrix[a][i-1];
 					}
-					
-					
-//					
-					
 					probabilities[j] = calculateProb(lastTownIndex, townsUnvisited[a][j],  pheromoneMatrix);
 				}
 				
 //				Evaluation.printMatrix(townsUnvisited);
 //				System.out.println();
-				
-				
+
 				//normalize probabilities
 				double normalization = 0.0;
 				//normalize all probabilities
@@ -123,8 +155,6 @@ public class SolutionGeneration {
 				}
 				 
 //				Evaluation.printArray(probabilities);
-
-				
 				// with probability of q just take the next town with the next probability
                 if (getRandomBoolean(greedyParameter)) {
                     solutionsMatrix[a][progress] = townsUnvisited[a][getMaxTown(probabilities)];
@@ -133,8 +163,6 @@ public class SolutionGeneration {
 
 				for (int j2 = 0; j2 < indexProbabilities; j2++) {
 					
-			
-
 					if (j2 == indexProbabilities - 1) {
 						end = choice.length;
 					} else {
@@ -142,8 +170,6 @@ public class SolutionGeneration {
 						end = (int) Math.round(probabilities[j2]*1000);
 					}	
 						
-					
-
 					//add current town choices, occurrence of current town depends and probability
 					for (int l = 0; l < end; l++) {
 
@@ -153,9 +179,6 @@ public class SolutionGeneration {
 						} 
 						counter++;
 					}
-					
-					
-				
 				}
 				
 //				System.out.println();
@@ -169,33 +192,21 @@ public class SolutionGeneration {
 				solutionsMatrix[a][progress] = choice[random.nextInt(choice.length)];
             }
 				
-				
-                
-                
-			
-				
 				for (int j = 0; j < indexLastUnvisitedTowns; j++) {
 					if (townsUnvisited[a][j] == solutionsMatrix[a][progress]) {
 						
-						
 						//delete chosen town from list of unvisited towns
 						swap(townsUnvisited[a], j, indexLastUnvisitedTowns - 1);
+						probabilities[indexProbabilities - 1] = 0;
+						townsUnvisited[a][indexLastUnvisitedTowns - 1] = 0;
 						break;
-				
 					}
-				}
-				
-				
-				
-					
+				}	
 			}
 			indexLastUnvisitedTowns--;
 			indexProbabilities--;
 			progress++;
-
 		}
-			
-		
 		return solutionsMatrix;
 	}
 	
@@ -203,21 +214,24 @@ public class SolutionGeneration {
 	
 	/**
 	 * Calculate probability depending on pheromoneValue, pheromoneWeight and heuristicWeights
-	 * @param i
-	 * @param j
+	 * @param coming from city index i
+	 * @param going to city index j
 	 * @return probability
 	 */
 	private double calculateProb(int i, int j,  double[][] pheromoneMatrix) {
-		
-		
-		double prob = Math.pow(pheromoneMatrix[i][j],pheromoneWeight)/Math.pow(distanceMatrix[i][j],heuristicWeights);
-		
-		
-		
-		
+		double prob = Math.pow(pheromoneMatrix[i][j], pheromoneWeight)/Math.pow(distanceMatrix[i][j], heuristicWeights);
 		return prob;
 	}
 	
+	
+	
+	/**
+	 * Method to swap array[i] and array[j]
+	 * @param array
+	 * @param index i
+	 * @param index j
+	 * @return new array
+	 */
 	private int[] swap(int[] array,int i, int j) {
 		int dummy;
 		dummy = array[i];
@@ -227,14 +241,23 @@ public class SolutionGeneration {
 		return array;
 	}
 	
+	
+	
+	/**
+	 * Method to get 'yes' with probability p and 'no' otherwise
+	 * @param p
+	 * @return random boolean
+	 */
 	public boolean getRandomBoolean(double p){
         return random.nextDouble() < p;
     }
 	
+	
+	
 	/**
-     * find maximum
+     * Method to find maximum in array
      * @param inputArray
-     * @return
+     * @return index of maximum town
      */
     private static int getMaxTown(double[] inputArray) {
         double maxValue = inputArray[0];
